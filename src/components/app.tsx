d164e859fbf6e320594a6dae02e1527efb2e1d7e
Erik Nelson <erik@nsk.io>
2016-06-25 09:20:32 -0400

    Add initial react demo classes

---
+++ src/components/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Hello from './hello'
+
+class App extends React.Component<any, any> {
+  render() {
+    return <Hello />
+  }
+}
+
+export default App;
---
+++ src/components/hello.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Hello extends React.Component<any, any> {
+  render() {
+    return (
+      <div>
+        <h1>This is nskbp</h1>
+        <p>foobar</p>
+      </div>
+    )
+  }
+}
+
+export default Hello;
